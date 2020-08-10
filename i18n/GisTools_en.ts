bbaf06a2bb742d458b58d0b2ed6436d14104056f
Aleksandr Prazdnikov <79237017153@ya.ru>
2020-08-10 15:37:57 +0700

    Added transliterations

---
+++ i18n/GisTools_en.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.ui" line="20"/>
+        <source>Converting GPX to MapInfo TAB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.ui" line="154"/>
+        <source>Select folder with files GPX format</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GisTools</name>
+    <message>
+        <location filename="../gistools.py" line="71"/>
+        <source>Gis Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gistools.py" line="166"/>
+        <source>Panel Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GisToolsDockWidgetBase</name>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_dockwidget_base.ui" line="14"/>
+        <source>Gis Tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>GpxTabDialog</name>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="49"/>
+        <source>Select folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="54"/>
+        <source>Convert all items to MapInfo TAB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="61"/>
+        <source>Convert selected items to MapInfo TAB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="68"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="75"/>
+        <source>File name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="83"/>
+        <source>Select folder with files GPX format</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="91"/>
+        <source>Select item or all items convert to MapInfo TAB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="104"/>
+        <source>You must select at least one file from the list</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/GisTools_ru.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0">
+<context>
+    <name>Dialog</name>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.ui" line="20"/>
+        <source>Converting GPX to MapInfo TAB</source>
+        <translation type="finished">Преобразование GPX в MapInfo TAB</translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.ui" line="154"/>
+        <source>Select folder with files GPX format</source>
+        <translation type="finished">Выберите директорию с файлами GPX формата</translation>
+    </message>
+</context>
+<context>
+    <name>GisTools</name>
+    <message>
+        <location filename="../gistools.py" line="71"/>
+        <source>Gis Tools</source>
+        <translation>ГИС Инструменты</translation>
+    </message>
+    <message>
+        <location filename="../gistools.py" line="166"/>
+        <source>Panel Tools</source>
+        <translation>Панель инструментов</translation>
+    </message>
+</context>
+<context>
+    <name>GisToolsDockWidgetBase</name>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_dockwidget_base.ui" line="14"/>
+        <source>Gis Tools</source>
+        <translation type="finished">Панель инструментов</translation>
+    </message>
+</context>
+<context>
+    <name>GpxTabDialog</name>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="91"/>
+        <source>Select item or all items convert to MapInfo TAB</source>
+        <translation type="finished">Выберите элемент или все элементы для преобразования в MapInfo TAB</translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="49"/>
+        <source>Select folder</source>
+        <translation type="finished">Выбрать директорию</translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="54"/>
+        <source>Convert all items to MapInfo TAB</source>
+        <translation type="finished">Преобразовать все элементы в MapInfo TAB</translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="61"/>
+        <source>Convert selected items to MapInfo TAB</source>
+        <translation type="finished">Преобразовать выбранные элементы в MapInfo TAB</translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="68"/>
+        <source>Cancel</source>
+        <translation type="finished">Отмена</translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="75"/>
+        <source>File name</source>
+        <translation type="finished">Название файла</translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="83"/>
+        <source>Select folder with files GPX format</source>
+        <translation type="finished">Выберите директорию с файлами GPX формата</translation>
+    </message>
+    <message>
+        <location filename="../gis_tools_gui/gis_tools_gpx_tab_dialog.py" line="104"/>
+        <source>You must select at least one file from the list</source>
+        <translation type="finished">Вам необходимо выбрать хотя бы один файл из списка</translation>
+    </message>
+</context>
+</TS>
